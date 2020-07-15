97000efcf9e278e6253c667065a909cfc78e1a60
collaedge <a125614418@outlook.com>
2020-07-15 13:11:32 +1000

    Update node.rs

--- src/node.rs
+++ src/node.rs
@@ -467,8 +467,7 @@ impl PoERNode {
         self.msg_log.add_message(msg);
 
         // if this node is primary, count vote, and commit
-        if info.get_seq_num() == state.seq_num && 
-                state.phase == PbftPhase::Committing && state.is_primary() {
+        if state.phase == PbftPhase::Committing && state.is_primary() {
             info!("===========primary handle commit===============");
             // The node is ready to commit the block (i.e. the predicate `committable` is true)
             // when its log has 2f + 1 Commit messages from different nodes that match the
